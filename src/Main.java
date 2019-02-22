8714559863b15724e120d36c94f188cec2c00c5b
A Junior <jr@ieee.org>
2019-02-22 14:09:08 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,61 @@
+import java.net.URI;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+public class Main {
+
+    private static final String subscriptionKey = "99359808137044469bf40791defd5049"; //"<Subscription Key>";
+
+    private static final String uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/analyze";
+
+    private static final String imageToAnalyze = "https://s2.glbimg.com/d55tITbwNlnnroNaO7Tf2d98Ec0=/0x0:637x356/1600x0/smart/filters:strip_icc()/s.glbimg.com/es/ge/f/original/2019/02/21/neymarpai.jpg";
+            //"https://upload.wikimedia.org/wikipedia/commons/" +
+              //      "1/12/Broadway_and_Times_Square_by_night.jpg";
+
+    public static void main(String[] args) {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        try {
+            URIBuilder builder = new URIBuilder(uriBase);
+
+            // Request parameters. All of them are optional.
+            builder.setParameter("visualFeatures", "Categories,Description,Color");
+            builder.setParameter("language", "en");
+
+            // Prepare the URI for the REST API method.
+            URI uri = builder.build();
+            HttpPost request = new HttpPost(uri);
+
+            // Request headers.
+            request.setHeader("Content-Type", "application/json");
+            request.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            // Request body.
+            StringEntity requestEntity =
+                    new StringEntity("{\"url\":\"" + imageToAnalyze + "\"}");
+            request.setEntity(requestEntity);
+
+            // Call the REST API method and get the response entity.
+            HttpResponse response = httpClient.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null) {
+                // Format and display the JSON response.
+                String jsonString = EntityUtils.toString(entity);
+                JSONObject json = new JSONObject(jsonString);
+                System.out.println("Resposta da API:\n");
+                System.out.println(json.toString(2));
+            }
+        } catch (Exception e) {
+            // Display error message.
+            System.out.println(e.getMessage());
+        }
+    }
+}
